b0740456fb72fdd03a0be2a6b35712a08d3195e5
Spencer-EMS <spencer@ems.ai>
2022-11-16 17:19:38 +0000

    refactoring totals to show net gain/loss

--- src/components/Totals/Totals.js
+++ src/components/Totals/Totals.js
@@ -27,11 +27,15 @@ const Totals = ({
     }) => {
 
     // const [ rentEquity, setRentEquity ] = useState(0);
-    const [ netGainRent, setNetGainRent ] = useState(0);
-    // const [ buyEquity, setBuyEquity ] = useState(0);
+    const [ netPositionRent, setNetPositionRent ] = useState(0);
+    const [ buyEquity, setBuyEquity ] = useState(0);
     const [ netGainBuy, setNetGainBuy ] = useState(0);
+    const [ netGainRent, setNetGainRent ] = useState(0);
+    const [ netPositionBuy, setNetPositionBuy ] = useState(0);
     const [ comparisonMessage, setComparisonMessage ] = useState("");
     const [ equityDelta, setEquityDelta ] = useState(0);
+    const [ rentInterestEarned, setRentInterestEarned ] = useState(0);
+    const [ buyInterestEarned, setBuyInterestEarned ] = useState(0);
 
     // TESTING
     // const [ buyRoi, setBuyRoi ] = useState(0);
@@ -46,10 +50,12 @@ const Totals = ({
         var accruedAmount;
         const remainingSavings = currentSavings - sumUpFrontCosts - depositAmount;
         accruedAmount = remainingSavings*((1+((aer/100)/12))**(12*fixedTerm));
+        const interestEarned = accruedAmount - remainingSavings;
         var rOverN = (aer/100)/12;
         var numerator = (saveBuy*(((1+(rOverN))**(12*fixedTerm))-1));
         accruedAmount = accruedAmount + (numerator/(rOverN));
         setAccruedSavingsBuy(accruedAmount);
+        setBuyInterestEarned(interestEarned);
     }, [fixedTerm, currentSavings, aer, sumUpFrontCosts, depositAmount, setAccruedSavingsBuy, saveBuy]);
 
     // Calculating Compound interest on RENT savings [Formula: A=(P(1+r/n)^nt)  +  ((PMT((1+r/n)**(nt))-1)/(r/n))]
@@ -58,11 +64,13 @@ const Totals = ({
         const remainingSavings = currentSavings - upFrontRentCost - securityDeposit;
         // console.log("remainingSavings", remainingSavings);
         accruedAmount = remainingSavings*((1+((aer/100)/12))**(12*fixedTerm));
+        const interestEarned = accruedAmount - remainingSavings;
         var rOverN = (aer/100)/12;
         var numerator = (saveRent*(((1+(rOverN))**(12*fixedTerm))-1));
         accruedAmount = accruedAmount + (numerator/rOverN);
         // console.log("accruedAmount", accruedAmount);
         setAccruedSavingsRent(accruedAmount);
+        setRentInterestEarned(interestEarned);
     }, [fixedTerm, currentSavings, aer, upFrontRentCost, securityDeposit, setAccruedSavingsRent, saveRent]);
     
     // useEffect(() => { // Rent Equity
@@ -70,20 +78,30 @@ const Totals = ({
     //     setRentEquity(totalRentEquity);
     // }, [securityDeposit, currentSavings, upFrontRentCost, accruedSavingsRent]);
 
-    // useEffect(() => { // Buy Equity
-    //     const totalBuyEquity = (depositAmount + capitalGains + capitalRepaid);
-    //     setBuyEquity(totalBuyEquity);
-    // }, [depositAmount, currentSavings, sumUpFrontCosts, accruedSavingsBuy, capitalGains, capitalRepaid]);
+    useEffect(() => { // Buy Equity
+        const totalBuyEquity = (depositAmount + capitalGains + capitalRepaid);
+        setBuyEquity(totalBuyEquity);
+    }, [depositAmount, currentSavings, sumUpFrontCosts, accruedSavingsBuy, capitalGains, capitalRepaid]);
 
-    useEffect(() => { // Net Gain/Loss Buy 
-        const netGainLoss = ((depositAmount + capitalGains + capitalRepaid) + accruedSavingsBuy);
+    useEffect(() => { // Net Position Buy 
+        const netPosition = ((depositAmount + capitalGains + capitalRepaid) + accruedSavingsBuy);
+        setNetPositionBuy(netPosition);
+    }, [depositAmount, capitalGains , capitalRepaid, accruedSavingsBuy, setNetPositionBuy]);
+
+    useEffect(() => { // Net gain/loss Buy
+        const netGainLoss = ( netPositionBuy - totalSpendBuy );
         setNetGainBuy(netGainLoss);
-    }, [depositAmount, capitalGains , capitalRepaid, accruedSavingsBuy, setNetGainBuy]);
+    }, [totalSpendBuy, netPositionBuy, setNetGainBuy]);
 
-    useEffect(() => { // Net Gain/Loss Rent 
-        const netGainLoss = ((securityDeposit) + accruedSavingsRent);
+    useEffect(() => { // Net Position Rent 
+        const netPosition = ((securityDeposit) + accruedSavingsRent);
+        setNetPositionRent(netPosition);
+    }, [securityDeposit, currentSavings, upFrontRentCost, accruedSavingsRent, setNetPositionRent]);
+
+    useEffect(() => { // Net gain/loss Rent
+        const netGainLoss = ( netPositionRent - totalSpendRent );
         setNetGainRent(netGainLoss);
-    }, [securityDeposit, currentSavings, upFrontRentCost, accruedSavingsRent, setNetGainRent]);
+    }, [totalSpendRent, netPositionRent, setNetGainRent]);
 
 
     // Rent vs Buy delta
@@ -131,31 +149,41 @@ const Totals = ({
                         <h3>£{equityDelta.toFixed(0)}</h3>
                     </div>
                     <h4>Buying</h4>
-                    <h5>Total Spend: £{totalSpendBuy.toFixed(0)}</h5>
+                    
                     <p>Upfront costs: £{sumUpFrontCosts.toFixed(0)}</p>
                     <p>Monthly costs: £{sumMonthlyCosts.toFixed(0)}</p>
                     <p>Interest cost: £{periodInterestCost.toFixed(0)}</p>
-                    <h5>Sunk costs: £{buyTotalCost.toFixed(0)}</h5>
+                    <h6>Sunk costs: £{buyTotalCost.toFixed(0)}</h6>
 
                     <p>Deposit: £{depositAmount.toFixed(0)}</p>
                     {/* <p>Remaining Savings: £{(currentSavings - sumUpFrontCosts - depositAmount).toFixed(0)}</p> */}
-                    
-                    <p>Capital gains: £{capitalGains.toFixed(0)}</p>
                     <p>Capital repaid: £{capitalRepaid.toFixed(0)}</p>
-                    {/* <h5>Equity: £{buyEquity.toFixed(0)}</h5> */}
-                    <p>Total Savings: £{accruedSavingsBuy.toFixed(0)}</p>
+                    <p>Capital gains: £{capitalGains.toFixed(0)}</p>
+                    <h6>Equity: £{buyEquity.toFixed(0)}</h6>
+
+                    <p>Amount saved: £{((currentSavings - upFrontRentCost - securityDeposit)+(saveBuy*(fixedTerm*12))).toFixed(0)}</p>
+                    <p>Interest earned: £{buyInterestEarned.toFixed(0)}</p>
+                    <h6>Total Savings: £{accruedSavingsBuy.toFixed(0)}</h6>
+                    <p>Total spend: £{totalSpendBuy.toFixed(0)}</p>
+                    <p>Net position: £{netPositionBuy.toFixed(0)}</p>
                     <h5>Net gain/loss: £{netGainBuy.toFixed(0)}</h5>
                     
                     <h4>Renting</h4>
-                    <h5>Total Spend: £{totalSpendRent.toFixed(0)}</h5>
+                    
                     <p>Upfront costs: £{upFrontRentCost}</p>
                     <p>Rent paid: £{rentMonthlyCost.toFixed(0)}</p>
                     <h5>Sunk costs: £{(upFrontRentCost + rentMonthlyCost).toFixed(0)}</h5>
 
-                    <p>Security Deposit: £{securityDeposit.toFixed(0)}</p>
+                    
                     {/* <p>Remaining Savings: £{(currentSavings - upFrontRentCost - securityDeposit).toFixed(0)}</p> */}
-                    <p>Total Savings: £{accruedSavingsRent.toFixed(0)}</p>
+                    <p>Amount saved: £{((currentSavings - upFrontRentCost - securityDeposit)+(saveRent*(fixedTerm*12))).toFixed(0)}</p>
+                    <p>Interest earned: £{rentInterestEarned.toFixed(0)}</p>
+                    <h6>Total savings: £{accruedSavingsRent.toFixed(0)}</h6>
+                    {/* <p>Security deposit: £{securityDeposit.toFixed(0)}</p> */}
+                    <p>Total Spend: £{totalSpendRent.toFixed(0)}</p>
+                    <p>Net position: £{netPositionRent.toFixed(0)}</p>
                     <h5>Net gain/loss: £{netGainRent.toFixed(0)}</h5>
+                    
                 </div>
             </div>
         </div>
